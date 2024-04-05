f810b66b3cc9bfaac8312fddda9144f4b96a3a20
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-04-05 18:08:04 +0300

    add home cmp and routing

--- btc-insights/src/app/core/core.module.ts
+++ btc-insights/src/app/core/core.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, RouterModule, SharedModule],
+  providers: [],
+  exports: [],
 })
-export class CoreModule { }
+export class CoreModule {}
