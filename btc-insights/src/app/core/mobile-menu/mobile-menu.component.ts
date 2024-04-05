0b0e811f804ea90a95b17aa7386f5ec4cd6dcae3
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-04-05 18:29:49 +0300

    add nav links logic and styles to mobile menu

--- btc-insights/src/app/core/mobile-menu/mobile-menu.component.ts
+++ btc-insights/src/app/core/mobile-menu/mobile-menu.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NAV_LINKS } from 'src/app/shared/helpers/navLinks';
+
 @Component({
   selector: 'app-mobile-menu',
   templateUrl: './mobile-menu.component.html',
   styleUrls: ['./mobile-menu.component.css'],
 })
 export class MobileMenuComponent {
+  navLinks = NAV_LINKS;
 }
