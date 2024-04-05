e4957833447462dbc10930889a6caee2944dc5a1
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-04-05 18:26:09 +0300

    add initial mobile menu styles

--- btc-insights/src/app/core/core.module.ts
+++ btc-insights/src/app/core/core.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { MobileMenuComponent } from './mobile-menu/mobile-menu.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [HomeComponent],
-  declarations: [HomeComponent, HeaderComponent],
+  declarations: [HomeComponent, HeaderComponent, MobileMenuComponent],
   imports: [CommonModule, RouterModule, SharedModule],
   providers: [],
   exports: [],
---
+++ btc-insights/src/app/core/mobile-menu/mobile-menu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-mobile-menu',
+  templateUrl: './mobile-menu.component.html',
+  styleUrls: ['./mobile-menu.component.css'],
+})
+export class MobileMenuComponent {
+}
