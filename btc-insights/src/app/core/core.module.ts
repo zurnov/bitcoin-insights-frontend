5e0fa3f04bdc763d1bfd622c8d5b75e18f81afff
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-04-05 18:20:28 +0300

    add initial responsive header styles

--- btc-insights/src/app/core/core.module.ts
+++ btc-insights/src/app/core/core.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HomeComponent],
+  declarations: [HomeComponent, HeaderComponent],
   imports: [CommonModule, RouterModule, SharedModule],
   providers: [],
   exports: [],
+  exports: [HeaderComponent],
 })
 export class CoreModule {}
