0f424de84f110d38255bdd1bce4377224ca1db6e
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-04-15 18:20:01 +0300

    implement blockchain info retrieval in insights service

--- src/app/insights/insights.service.ts
+++ src/app/insights/insights.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IBlockchainInfo } from '../shared/interfaces/blockchainInfo';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InsightsService {
-  constructor() {}
+  private baseUrl = 'https://www.zurnov.com/api/v1/btc-insights';
+
+  constructor(private http: HttpClient) {}
+
+  getBlockchainInfo(): Observable<IBlockchainInfo> {
+    return this.http.get<IBlockchainInfo>(`${this.baseUrl}/getblockchaininfo`);
+  }
 }
