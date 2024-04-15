59f9acca030ca4a5a52fe2ac885903d97d17ce8d
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-04-15 20:14:20 +0300

    implement auto refresh for blockchain info every 10m

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgxTimelineEvent, NgxTimelineItemPosition } from '@frxjs/ngx-timeline';
+import { Subscription, interval } from 'rxjs';
 import { InsightsService } from 'src/app/insights/insights.service';
 import { IBlockchainInfo } from 'src/app/shared/interfaces/blockchainInfo';
 
@@ -8,7 +9,10 @@ import { IBlockchainInfo } from 'src/app/shared/interfaces/blockchainInfo';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  tenMinsInMilliseconds = 10 * 60 * 1000;
+  private refreshSubscription!: Subscription;
+
   blockchainInfo: IBlockchainInfo | undefined;
   btcTimelineEvents: NgxTimelineEvent[] = [
     {
@@ -80,10 +84,26 @@ export class HomeComponent implements OnInit {
   constructor(private insightsService: InsightsService) {}
 
   ngOnInit(): void {
+    let fetchCounter = 1;
+    this.fetchBlockchainInfo(fetchCounter);
+
+    this.refreshSubscription = interval(this.tenMinsInMilliseconds).subscribe(
+      () => {
+        fetchCounter++;
+        this.fetchBlockchainInfo(fetchCounter);
+      }
+    );
+  }
+
+  fetchBlockchainInfo(fetchCounter: number) {
     this.insightsService.getBlockchainInfo().subscribe({
       next: (data: IBlockchainInfo) => {
         this.blockchainInfo = data;
-        // console.log('Blockchain info fetched:', this.blockchainInfo);
+
+        console.log(
+          `Blockchain info fetched for the ${fetchCounter} time:`,
+          this.blockchainInfo
+        );
       },
       error: (err: Error) => {
         console.error('Error fetching blockchain info:', err);
@@ -107,5 +127,9 @@ export class HomeComponent implements OnInit {
       'Dec',
     ];
     return months[month];
+  }
+
+  ngOnDestroy(): void {
+    this.refreshSubscription?.unsubscribe();
   }
 }
