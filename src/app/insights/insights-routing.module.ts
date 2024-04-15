7e0ad07dd1a683962f5d33f37ecdc8849ed68f90
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-04-15 18:27:59 +0300

    add child routing for address-specific insights

--- src/app/insights/insights-routing.module.ts
+++ src/app/insights/insights-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResultsComponent } from './results/results.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ':address',
+    component: ResultsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
