cf6a57d6d6703abbeb8699406ee853d2f28ba3f0
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-04-15 18:53:39 +0300

    integrate address balance & history services into results cmp

--- src/app/insights/results/results.component.ts
+++ src/app/insights/results/results.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { InsightsService } from '../insights.service';
+import { IAddressHistory } from 'src/app/shared/interfaces/addressHistory';
+import { IAddressBalance } from 'src/app/shared/interfaces/addressBalance';
 
 @Component({
   selector: 'app-results',
@@ -6,6 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './results.component.css',
 })
 export class ResultsComponent implements OnInit {
+  addressBalance: IAddressBalance | undefined;
+  addressHistory: IAddressHistory | undefined;
+
+  constructor(private insightsService: InsightsService) {}
+
   ngOnInit(): void {
+    //!static addresses
+    this.insightsService
+      .getAddressBalance('bc1q8yj0herd4r4yxszw3nkfvt53433thk0f5qst4g')
+      .subscribe({
+        next: (data: IAddressBalance) => {
+          this.addressBalance = data;
+          // console.log('Address balance fetched:', this.addressBalance);
+        },
+        error: (err: Error) => {
+          console.error('Error fetching address balance:', err);
+        },
+      });
+
+    this.insightsService
+      .getAddressHistory('34xp4vRoCGJym3xR7yCVPFHoCNxv4Twseo')
+      .subscribe({
+        next: (data: IAddressHistory) => {
+          this.addressHistory = data;
+          // console.log('Address history fetched:', this.addressHistory);
+        },
+        error: (err: Error) => {
+          console.error('Error fetching address history:', err);
+        },
+      });
   }
 }
