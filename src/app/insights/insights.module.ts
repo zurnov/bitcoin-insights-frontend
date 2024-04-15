7d4af14519b92ef8b704228c4bfd39e0cac8e78c
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-04-15 18:23:32 +0300

    add search results cmp in insights module

--- src/app/insights/insights.module.ts
+++ src/app/insights/insights.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InsightsRoutingModule } from './insights-routing.module';
+import { ResultsComponent } from './results/results.component';
+import { HttpClientModule } from '@angular/common/http';
+import { InsightsService } from './insights.service';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, InsightsRoutingModule],
+  declarations: [ResultsComponent],
+  imports: [CommonModule, InsightsRoutingModule, HttpClientModule],
+  providers: [InsightsService],
 })
 export class InsightsModule {}
---
+++ src/app/insights/results/results.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-results',
+  templateUrl: './results.component.html',
+  styleUrl: './results.component.css',
+})
+export class ResultsComponent implements OnInit {
+  ngOnInit(): void {
+  }
+}
