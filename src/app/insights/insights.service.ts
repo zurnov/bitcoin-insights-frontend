f503c81156737d91b83266eccae744444f7a75a0
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-04-15 18:20:29 +0300

    implement address history retrieval in insights service

--- src/app/insights/insights.service.ts
+++ src/app/insights/insights.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { IBlockchainInfo } from '../shared/interfaces/blockchainInfo';
 import { Observable } from 'rxjs';
 import { IAddressBalance } from '../shared/interfaces/addressBalance';
+import { IAddressHistory } from '../shared/interfaces/addressHistory';
 
 @Injectable({
   providedIn: 'root',
@@ -19,6 +20,12 @@ export class InsightsService {
   getAddressBalance(address: string): Observable<IAddressBalance> {
     return this.http.get<IAddressBalance>(
       `${this.baseUrl}/getaddressbalance/${address}`
+    );
+  }
+
+  getAddressHistory(address: string): Observable<IAddressHistory> {
+    return this.http.get<IAddressHistory>(
+      `${this.baseUrl}/getaddresshistory/${address}`
     );
   }
 }
