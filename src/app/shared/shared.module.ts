57cdacf554098cb2f49509b04be7655f24508dba
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-04-16 21:17:13 +0300

    add pre-fetching spinners to home cmp structure

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HashRateConversionPipe } from './pipes/hash-rate-conversion.pipe';
 import { DifficultyConversionPipe } from './pipes/difficulty-conversion.pipe';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
-  declarations: [HashRateConversionPipe, DifficultyConversionPipe],
+  declarations: [
+    HashRateConversionPipe,
+    DifficultyConversionPipe,
+    SpinnerComponent,
+  ],
   imports: [CommonModule],
-  exports: [HashRateConversionPipe, DifficultyConversionPipe],
+  exports: [HashRateConversionPipe, DifficultyConversionPipe, SpinnerComponent],
 })
 export class SharedModule {}
