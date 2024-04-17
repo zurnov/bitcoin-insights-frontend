22de92dd955e44760ff2fb57bd3c474d6067c9cb
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-04-17 14:58:12 +0300

    implement shared responsive pagination controls

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,22 @@ import { CommonModule } from '@angular/common';
 import { HashRateConversionPipe } from './pipes/hash-rate-conversion.pipe';
 import { DifficultyConversionPipe } from './pipes/difficulty-conversion.pipe';
 import { SpinnerComponent } from './components/spinner/spinner.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     HashRateConversionPipe,
     DifficultyConversionPipe,
     SpinnerComponent,
+    PaginationComponent,
   ],
-  imports: [CommonModule],
-  exports: [HashRateConversionPipe, DifficultyConversionPipe, SpinnerComponent],
+  imports: [CommonModule,FormsModule],
+  exports: [
+    HashRateConversionPipe,
+    DifficultyConversionPipe,
+    SpinnerComponent,
+    PaginationComponent,
+  ],
 })
 export class SharedModule {}
