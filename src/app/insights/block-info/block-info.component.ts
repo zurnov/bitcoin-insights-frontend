3439100462376ad6b128084fc21e1a57dedfd966
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-04-17 12:40:25 +0300

    implement data retrieval in block info cmp

--- src/app/insights/block-info/block-info.component.ts
+++ src/app/insights/block-info/block-info.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { InsightsService } from '../insights.service';
+import { IBlockInfo } from 'src/app/shared/interfaces/blockInfo';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-block-info',
@@ -6,4 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './block-info.component.css',
 })
 export class BlockInfoComponent implements OnInit {
+  blockInfo: IBlockInfo | undefined;
+  blockHeight: number | null = null;
+
+  constructor(
+    private insightsService: InsightsService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.blockHeight = Number(this.route.snapshot.paramMap.get('height'));
+    if (!this.blockHeight) {
+      return;
+    }
+
+    this.insightsService.getBlockInfoByHeight(this.blockHeight).subscribe({
+      next: (data: IBlockInfo) => {
+        this.blockInfo = data;
+        // console.log('Block info fetched:', this.blockInfo);
+      },
+      error: (err: Error) => {
+        console.error('Error fetching block info:', err);
+      },
+    });
+  }
 }
