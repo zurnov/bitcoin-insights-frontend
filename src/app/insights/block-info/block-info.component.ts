4a6947320cd872f86f86cd4166702f9f489f1195
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-04-17 21:39:50 +0300

    redirect to transaction info on block info txHash click
    - removes unnecessary anchor tag from current Hash

--- src/app/insights/block-info/block-info.component.ts
+++ src/app/insights/block-info/block-info.component.ts
@@ -33,4 +33,11 @@ export class BlockInfoComponent implements OnInit {
       },
     });
   }
+
+  onTxHashClick(event: MouseEvent) {
+    const target = event.currentTarget as HTMLElement;
+    const txHashContent = target.textContent?.trim();
+
+    this.router.navigate(['/insights/transaction', txHashContent]);
+  }
 }
