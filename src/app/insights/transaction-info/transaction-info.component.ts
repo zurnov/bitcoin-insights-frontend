5dec7f76dbdecf91c144e660a438d09a35b8467a
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-04-17 22:46:32 +0300

    integrate getTransactionInfo service in transaction info cmp

--- src/app/insights/transaction-info/transaction-info.component.ts
+++ src/app/insights/transaction-info/transaction-info.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ITransactionInfo } from 'src/app/shared/interfaces/transactionInfo';
+import { InsightsService } from '../insights.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-transaction-info',
@@ -6,4 +9,29 @@ import { Component } from '@angular/core';
   styleUrl: './transaction-info.component.css',
 })
 export class TransactionInfoComponent {
+  transactionInfo: ITransactionInfo | undefined;
+  txHash!: string;
+
+  constructor(
+    private insightsService: InsightsService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.txHash = this.route.snapshot.paramMap.get('txHash') || '';
+    if (!this.txHash) {
+      return;
+    }
+
+    this.insightsService.getTransactionInfo(this.txHash).subscribe({
+      next: (data: ITransactionInfo) => {
+        this.transactionInfo = data;
+
+        console.log('Transaction info fetched:', this.transactionInfo);
+      },
+      error: (err: Error) => {
+        console.error('Error fetching transaction info:', err);
+      },
+    });
+  }
 }
