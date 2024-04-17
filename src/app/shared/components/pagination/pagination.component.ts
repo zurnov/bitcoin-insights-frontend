f430f163f872cb3b09f4c3a6c54cee2a85ae6da2
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-04-17 15:07:48 +0300

    implement page change event emitter for parent communication
    - integrated app-pagination in results cmp

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -8,4 +8,15 @@ import { Component, Input } from '@angular/core';
 export class PaginationComponent {
   @Input() currentPage: number = 1;
   @Input() totalPages: number = 10;
+  @Output() pageChange: EventEmitter<number> = new EventEmitter<number>();
+
+  handlePageChange(pageOrAction: number | string): void {
+    if (typeof pageOrAction === 'number') {
+      this.pageChange.emit(pageOrAction);
+    } else if (pageOrAction === 'prev' && this.currentPage > 1) {
+      this.pageChange.emit(this.currentPage - 1);
+    } else if (pageOrAction === 'next' && this.currentPage < this.totalPages) {
+      this.pageChange.emit(this.currentPage + 1);
+    }
+  }
 }
