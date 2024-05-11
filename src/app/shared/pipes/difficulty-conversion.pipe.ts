449b843e9112653c37df34cf1f79ea950e573a52
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-05-11 12:17:34 +0300

    refine table presentation for latest blocks/TXs

--- src/app/shared/pipes/difficulty-conversion.pipe.ts
+++ src/app/shared/pipes/difficulty-conversion.pipe.ts
@@ -6,6 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DifficultyConversionPipe implements PipeTransform {
   transform(val: number): string {
     const trillions = val / Math.pow(10, 12);
-    return trillions.toFixed(2) + ' T';
+    return trillions.toFixed(2);
   }
 }
