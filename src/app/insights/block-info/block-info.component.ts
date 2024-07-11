a09213953af7e7a45cb17547b9fe83c23ad05183
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-07-11 09:50:18 +0300

    fetch tx details for block info TXs

--- src/app/insights/block-info/block-info.component.ts
+++ src/app/insights/block-info/block-info.component.ts
@@ -2,7 +2,8 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { InsightsService } from '../insights.service';
 import { IBlockInfo } from 'src/app/shared/interfaces/blockInfo';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { Subject, filter, takeUntil } from 'rxjs';
+import { Subject, filter, forkJoin, takeUntil } from 'rxjs';
+import { ITransactionInfo } from 'src/app/shared/interfaces/transactionInfo';
 
 @Component({
   selector: 'app-block-info',
@@ -11,6 +12,7 @@ import { Subject, filter, takeUntil } from 'rxjs';
 })
 export class BlockInfoComponent implements OnInit, OnDestroy {
   blockInfo: IBlockInfo | undefined;
+  transactionDetails: ITransactionInfo[] = [];
   blockParam: string | null = null;
   currentPage: number = 1;
   totalPages!: number;
@@ -32,39 +34,30 @@ export class BlockInfoComponent implements OnInit, OnDestroy {
     this.route.queryParams.subscribe((params) => {
       this.currentPage = +params['page'] || 1;
 
-      if (isNaN(+this.blockParam!)) {
-        this.insightsService
-          .getBlockInfoByHash(this.blockParam!, this.currentPage)
-          .subscribe({
-            next: (data: IBlockInfo) => {
-              this.blockInfo = data;
-              this.totalPages = Math.ceil(data.ntx / 10);
+      const fetchBlockInfo = isNaN(+this.blockParam!)
+        ? this.insightsService.getBlockInfoByHash(
+            this.blockParam!,
+            this.currentPage
+          )
+        : this.insightsService.getBlockInfoByHeight(
+            +this.blockParam!,
+            this.currentPage
+          );
 
-              // console.log('Block info by hash fetched:', this.blockInfo);
-            },
-            error: (err: Error) => {
-              console.error('Error fetching block info:', err);
-              this.currentPage = 1;
-              this.updateRoute();
-            },
-          });
-      } else {
-        this.insightsService
-          .getBlockInfoByHeight(+this.blockParam!, this.currentPage)
-          .subscribe({
-            next: (data: IBlockInfo) => {
-              this.blockInfo = data;
-              this.totalPages = Math.ceil(data.ntx / 10);
+      fetchBlockInfo.subscribe({
+        next: (data: IBlockInfo) => {
+          this.blockInfo = data;
 
-              // console.log('Block info by height fetched:', this.blockInfo);
-            },
-            error: (err: Error) => {
-              console.error('Error fetching block info:', err);
-              this.currentPage = 1;
-              this.updateRoute();
-            },
-          });
-      }
+          this.totalPages = Math.ceil(data.ntx / 10);
+
+          this.fetchTransactionDetails();
+        },
+        error: (err: Error) => {
+          console.error('Error fetching block info:', err);
+          this.currentPage = 1;
+          this.updateRoute();
+        },
+      });
     });
 
     this.router.events
@@ -75,6 +68,61 @@ export class BlockInfoComponent implements OnInit, OnDestroy {
       .subscribe(() => {
         window.scrollTo(0, 1300);
       });
+  }
+
+  fetchTransactionDetails(): void {
+    if (
+      !this.blockInfo?.transactions ||
+      this.blockInfo.transactions.length === 0
+    ) {
+      return;
+    }
+
+    const requests = this.blockInfo.transactions.map((txId) => {
+      return this.insightsService.getTransactionInfo(txId);
+    });
+
+    forkJoin(requests).subscribe({
+      next: (results: any[]) => {
+        this.transactionDetails = results.map((txDetails, index) => {
+          let totalSent = 0;
+          txDetails.vout.forEach((output: any) => {
+            if (
+              output.scriptPubKey &&
+              output.scriptPubKey.type !== 'nulldata'
+            ) {
+              totalSent += output.value;
+            }
+          });
+
+          return {
+            txId: this.blockInfo!.transactions[index],
+            hash: txDetails.hash,
+            size: txDetails.size,
+            weight: txDetails.weight,
+            version: txDetails.version,
+            lockTime: txDetails.lockTime,
+            vin: txDetails.vin,
+            vout: txDetails.vout,
+            hex: txDetails.hex,
+            blockHash: txDetails.blockHash,
+            confirmations: txDetails.confirmations,
+            blockTime: new Date(txDetails.blockTime * 1000),
+            time: new Date(txDetails.time * 1000),
+            vbytes: txDetails.vbytes,
+            direction: {
+              receivedAmount: 0,
+              sentAmount: totalSent,
+            },
+          };
+        });
+
+        console.log(this.transactionDetails);
+      },
+      error: (err: Error) => {
+        console.error('Error fetching transaction details:', err);
+      },
+    });
   }
 
   onPageChange(page: number): void {
