060a2c0c8bedb08506c26ec1ea00710257efbe39
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-07-21 15:01:37 +0300

    update donate page with banner & BTC/ETH options

--- src/app/core/donate/donate.component.ts
+++ src/app/core/donate/donate.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { ClipboardService } from 'ngx-clipboard';
+import { LoadingService } from 'src/app/shared/services/loading.service';
 
 @Component({
   selector: 'app-donate',
   templateUrl: './donate.component.html',
-  styleUrl: './donate.component.css'
+  styleUrl: './donate.component.css',
 })
 export class DonateComponent {
+  btcAddress = 'bc1q409g2sesns7069w2vqz8mjfdtzwqk4r47snrxk';
+  ethAddress = '0xD3C22260D052eaFab37E571eA0B9cdf57EA55a4C';
 
+  copiedAddress: 'btc' | 'eth' | null = null;
+
+  constructor(
+    private loadingService: LoadingService,
+    private cbService: ClipboardService
+  ) {
+    this.loadingService.hide();
+  }
+
+  copyAddress(address: string, type: 'btc' | 'eth') {
+    this.cbService.copy(address);
+    this.copiedAddress = type;
+
+    setTimeout(() => {
+      this.copiedAddress = null;
+    }, 2000);
+  }
 }
