65de09f0bfd8689c8031567c6cb0e5e9e71fca50
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-07-21 11:36:52 +0300

    hide footer during loading states

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { LoadingService } from 'src/app/shared/services/loading.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
+  isLoading$ = this.loadingService.isLoading$;
 
+  constructor(private loadingService: LoadingService) {}
 }
