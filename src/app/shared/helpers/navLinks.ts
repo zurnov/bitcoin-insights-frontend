633ab9f443f6a7f067d100e812d2e1a82099914a
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-07-21 13:27:43 +0300

    update menu nav links

--- src/app/shared/helpers/navLinks.ts
+++ src/app/shared/helpers/navLinks.ts
@@ -2,6 +2,11 @@ import { INavLink } from '../interfaces/navLink';
 
 export const NAV_LINKS: INavLink[] = [
   { label: 'Home', route: '/' },
-  { label: 'Search', route: '/search' },
-  { label: 'About', route: '/about' },
+  { label: 'Donate', route: '/donate' },
+  {
+    label: 'GitHub Star',
+    route: 'https://github.com/zurnov/bitcoin-insights-frontend/',
+    external: true,
+    icon: 'fas fa-star',
+  },
 ];
