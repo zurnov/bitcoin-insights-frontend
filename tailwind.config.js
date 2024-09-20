20c7acf97440a0b166afd343bb122d3fecd1cc05
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-09-20 20:22:52 +0300

    handle pending TX to show status & hide missing info

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        'max-menu-w': '2400px',
+        "max-menu-w": "2400px",
+      },
+      keyframes: {
+        pulse: {
+          "0%, 100%": { opacity: "1" },
+          "50%": { opacity: "0.8" },
+        },
+      },
+      animation: {
+        "pulse-slow": "pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite",
       },
     },
   },
