7d761784cf5b3b6aaa553d31af0cce07800e00a3
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-09-24 17:11:47 +0300

    fix logo positioning issue in menu

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,10 @@
-import { Component, HostListener } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  ViewChild,
+} from '@angular/core';
 import { NAV_LINKS } from 'src/app/shared/helpers/navLinks';
 
 @Component({
@@ -6,19 +12,46 @@ import { NAV_LINKS } from 'src/app/shared/helpers/navLinks';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
   isMobileMenuOpen = false;
   navLinks = NAV_LINKS;
+  isButtonVisible = false;
+
+  @ViewChild('burgerBtn', { static: false }) burgerBtn!: ElementRef;
+
+  observer!: IntersectionObserver;
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     if (this.isMobileMenuOpen) {
-      this.isMobileMenuOpen = false;
+      // this.isMobileMenuOpen = false;
     }
   }
 
+  ngAfterViewInit() {
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        this.isButtonVisible = entries[0].isIntersecting;
+      },
+      { threshold: 1.0 }
+    );
+
+    this.observer.observe(this.burgerBtn.nativeElement);
+  }
+
   openMobileMenu() {
-    this.isMobileMenuOpen = true;
+    if (this.isButtonVisible) {
+      this.isMobileMenuOpen = true;
+    } else {
+      // console.log('burger btn not fully visible');
+
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+
+      this.isMobileMenuOpen = true;
+    }
   }
 
   handleCloseMobileMenu(value: boolean) {
@@ -26,6 +59,10 @@ export class HeaderComponent {
   }
 
   ngOnDestroy() {
+    if (this.observer && this.burgerBtn) {
+      this.observer.unobserve(this.burgerBtn.nativeElement);
+    }
+
     window.removeEventListener('scroll', this.onWindowScroll);
   }
 }
