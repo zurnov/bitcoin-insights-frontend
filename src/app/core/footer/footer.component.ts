53806622e722a20352b381515302453cca6af738
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-10-30 23:24:59 +0200

    implement smooth scrolling to dropdown on toggle

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { LoadingService } from 'src/app/shared/services/loading.service';
 
 @Component({
@@ -8,11 +8,29 @@ import { LoadingService } from 'src/app/shared/services/loading.service';
 })
 export class FooterComponent {
   isLoading$ = this.loadingService.isLoading$;
-  showDropdown: string | null = null; 
+  showDropdown: string | null = null;
+
+  @ViewChild('linkedinDropdown', { static: false })
+  linkedinDropdown!: ElementRef;
+  @ViewChild('githubDropdown', { static: false }) githubDropdown!: ElementRef;
 
   constructor(private loadingService: LoadingService) {}
 
   toggleDropdown(dropdown: string) {
     this.showDropdown = this.showDropdown === dropdown ? null : dropdown;
+
+    setTimeout(() => {
+      if (this.showDropdown === 'linkedin' && this.linkedinDropdown) {
+        this.linkedinDropdown.nativeElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+      } else if (this.showDropdown === 'github' && this.githubDropdown) {
+        this.githubDropdown.nativeElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+      }
+    }, 0);
   }
 }
