0254d1996b1e7151461d0e2a75ff0eb146a43273
Dimitar Zurnov <55513656+zurnov@users.noreply.github.com>
2026-04-28 15:32:39 +0300

    Switch baseUrl to localhost for development

--- src/app/insights/insights.service.ts
+++ src/app/insights/insights.service.ts
@@ -11,8 +11,8 @@ import { ITransactionInfo } from '../shared/interfaces/transactionInfo';
   providedIn: 'root',
 })
 export class InsightsService {
-  // private baseUrl: string = 'http://localhost:8000/api/v1/btc-insights';
-  private baseUrl: string = 'https://api.explore21.com/api/v1/btc-insights';
+  private baseUrl: string = 'http://localhost:8000/api/v1/btc-insights';
+  // private baseUrl: string = 'https://api.explore21.com/api/v1/btc-insights';
   private blockchainInfoCache$?: Observable<IBlockchainInfo>;
   private lastBlockchainInfoFetch: number = 0;
 
