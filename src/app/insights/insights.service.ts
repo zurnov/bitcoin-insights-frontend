d82fd4b7c212280dcfa024f49add24a6a42b69b1
Dimitar Zurnov <mitkozurnov@gmail.com>
2026-06-15 22:37:12 +0300

    Update baseUrl in InsightsService to use relative path and configure Nginx for API proxy

--- src/app/insights/insights.service.ts
+++ src/app/insights/insights.service.ts
@@ -12,7 +12,7 @@ import { ITransactionInfo } from '../shared/interfaces/transactionInfo';
 })
 export class InsightsService {
   // private baseUrl: string = 'http://localhost:8000/api/v1/btc-insights';
-  private baseUrl: string = 'https://api.explore21.com/api/v1/btc-insights';
+  private baseUrl: string = '/api/v1/btc-insights';
   private blockchainInfoCache$?: Observable<IBlockchainInfo>;
   private lastBlockchainInfoFetch: number = 0;
 
